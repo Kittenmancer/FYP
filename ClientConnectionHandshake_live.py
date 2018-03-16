cd3fe9a0f15a416ef5f22409f1ed2b092ac7c9b2
Kittenmancer <chris.taylor352@gmail.com>
2018-03-16 12:17:29 +0000

    Client side Python files

---
+++ ClientConnectionHandshake_live.py
@@ -0,0 +1,71 @@
+
+#christopher taylor 07/03/18 using code from Ben Knisley found at: https://gist.github.com/BenKnisley/5647884
+import socket, time
+import diffiehellmanv2_live as dh
+
+public = dh.get_publickey()
+
+def Tcp_connect( HostIp, Port ):
+    global s
+    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    s.connect((HostIp, Port))
+    return
+   
+def Tcp_Write(D):
+    temp_s = (D + '\r')
+    s.send(temp_s.encode('utf-8'))
+    return 
+   
+def Tcp_Read():
+	a = ' '
+	b = ''
+	while a != '\r':
+		a = bytes.decode(s.recv(1))
+		b = b + a
+	return b
+
+def Tcp_Close( ):
+   s.close() 
+   
+
+Tcp_connect( '192.168.0.19', 17098)
+Tcp_Write(str(public))
+shared = dh.get_sharedkey(public, int(Tcp_Read()))
+#Tcp_Write(str(shared))
+print(shared)
+print('closing connection')
+Tcp_Close()
+print('connection closed')
+
+#import sys
+#from socket import socket, AF_INET, SOCK_DGRAM
+#
+#SERVER_IP   = 
+#PORT_NUMBER = 5000
+#SIZE = 1024
+#
+#import time
+#import diffiehellmanv2_live as dh
+#
+#secret = 0
+#recieved = False
+#mySocket = socket( AF_INET, SOCK_DGRAM )
+#mySocket.connect((SERVER_IP,PORT_NUMBER))
+#myMessage = dh.get_publickey()
+#
+#mySocket.send(str(myMessage).encode('utf-8'))
+#mySocket.close()
+#mySocket.connect((SERVER_IP,PORT_NUMBER))
+#while recieved == False:
+#
+#(data,addr) = mySocket.recv(SIZE)
+#secret = dh.get_sharedkey(int(data))
+#print(secret)
+#print(data)
+#mySocket.close()    
+#
+#mySocket.sendto(myMessage1.encode('utf-8'),(SERVER_IP,PORT_NUMBER))
+#
+#sys.exit()
+
+#!/usr/bin/env python
---
+++ diffiehellmanv2_live.py
@@ -0,0 +1,24 @@
+# use Python 3 print function
+# this allows this code to run on python 2.x and 3.x
+from __future__ import print_function
+import random
+import numpy as np
+import os
+
+sharedPrime = 0xB10B8F96A080E01DDE92DE5EAE5D54EC52C99FBCFB06A3C69A6A9DCA52D23B616073E28675A23D189838EF1E2EE652C013ECB4AEA906112324975C3CD49B83BFACCBDD7D90C4BD7098488E9C219A73724EFFD6FAE5644738FAA31A4FF55BCCC0A151AF5F0DC8B4BD45BF37DF365C1A65E68CFDA76D4DA708DF1FB2BC2E4A4371
+sharedBase = 2
+
+def get_publickey():
+    a = int.from_bytes(os.urandom(1), byteorder='big')
+     # a
+# Alice Sends Bob A = g^a mod p
+    return(sharedBase**a) % sharedPrime
+
+def get_sharedkey(a, B):
+# Alice Computes Shared Secret: s = B^a mod p
+
+    x = (B ** a) % sharedPrime
+
+    print(x)
+    return x
+
